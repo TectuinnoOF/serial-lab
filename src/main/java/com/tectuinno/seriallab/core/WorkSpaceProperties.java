9aa883d601463ec05a19694c20d20b16e8396169
Pablo-Gomez-Perez <pablojesus34@hotmail.com>
2026-01-25 23:40:14 -0600

    cambio en formato JSpinner Stopbits

---
+++ src/main/java/com/tectuinno/seriallab/core/WorkSpaceProperties.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tectuinno.seriallab.core;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author root
+ */
+public class WorkSpaceProperties implements Serializable {
+    
+}
